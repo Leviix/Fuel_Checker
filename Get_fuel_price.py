c289c44821e4c4b9b00d396268d20c5553174f16
leviix <david27watters@gmail.com>
2018-11-28 11:43:20 +0800

    work on the script with Robin on the study sesh and fixed the exit command with sys.exit, also fixed the local test file *Macbook*

--- Get_fuel_price.py
+++ Get_fuel_price.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import sys
 import feedparser
 from bs4 import BeautifulSoup
 from pprint import pprint
@@ -8,15 +9,11 @@ from time import localtime
 
 Fuelinfo_today = 'https://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=2'
 Fuelinfo_tomorrow = 'https://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=2&Day=tomorrow'
-FUELTEST = 'file:///Users/leviix/Desktop/Scripts/Python_scripts/Cheap_fuel/Fuelinformation.html' #open('Fuelinformation.html','r+')
+FUELTEST = 'file:///Users/leviix/Desktop/Scripts/Python_scripts/Cheap_fuel/Fuelinformation.html'
 
 ### selector/menu
 def menu():
     """A menu for the user to interact with, continous until a correct selection has been made"""
-    # Fuelinfo_today = 'https://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=2'
-    # Fuelinfo_tomorrow = 'https://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=2&Day=tomorrow'
-    # FUELTEST = open('/Users/leviix/Desktop/Scripts/Python_scripts/Cheap_fuel/Fuelinformation.html', mode='w')
-
     os.system('clear')
     while True:
         try:
@@ -27,68 +24,52 @@ def menu():
                 print('Getting todays fuel prices...')
                 selected_data_set = Fuelinfo_today
                 return selected_data_set
-                # break
 
             elif selector == 2:
                 selected_data_set = Fuelinfo_tomorrow
                 print('Getting tomorrows fuel prices...')
                 return selected_data_set
-                # break
 
             elif selector == 3:
                 print('Getting both fuel prices...')
                 selected_data_set = [Fuelinfo_today, Fuelinfo_tomorrow]
                 return selected_data_set
-                # break
 
             elif selector == 4:
                 print('Getting Test (localhost) fuel prices...')
                 selected_data_set = FUELTEST
                 return selected_data_set
-                # break
 
             elif selector == 5:
-                # except InvalidURL
-                noreturn = ('http:///None?')
-                return noreturn
-                # break
+                sys.exit(0)
             else:
                 print('Input Error: Please select 1 - 5')
         except ValueError:
             print('Value Error: Please select 1 - 5')
             continue
-        except ConnectionError:
-            print('Not connected to the internet, check connection and try again')
+        # except ConnectionError:
+            # print('Not connected to the internet, check connection and try again')
 # menu()
 ###
-# print('THIS IS THE PRINT STATEMENT', menu(), "second time maybe #so confused",)
-both_data_sets = []
+
+combined_sets = []
 def get_data():
     selected_data_set = menu()
     if selected_data_set == FUELTEST:
         print(selected_data_set)
         parsed_info = feedparser.parse(FUELTEST)
-        # return parsed_info
+
+    elif selected_data_set == []:
+        pass
+
+
     else:
         grabbed_data = requests.get(selected_data_set)
         parsed_info = feedparser.parse(grabbed_data.text)
-        # return parsed_info
 
     for entries in parsed_info.entries:
-        print(entries.title) #',', entries.address)
-        both_data_sets.append(entries.title)
-
-### change atibute to selected_data_set when menu is working
-# grabbed_data = requests.get(selected_data_set)
-
-
-### Put any grabbed data through a parser to read and manipulate it better
-# parsed_info = feedparser.parse(grabbed_data.text)
-
-
-###
-# Sting manipulation
-
+        print(entries.title, entries.address)
+        combined_sets.append(entries.title)
 
 get_data()
 
@@ -96,39 +77,14 @@ get_data()
 # for entries in fuel_Formatted(grabbed_data).entries:
 
 print('--------------------     ENDED    --------------------')
-print(localtime().tm_hour)
-print(localtime().tm_min)
-print(localtime().tm_hour,':',localtime().tm_min)
+# print(localtime().tm_hour)
+# print(localtime().tm_min)
+hour = localtime().tm_hour
+minute = localtime().tm_min
+print('H',hour,': M', minute)
 
-if time <= '2 29':
-    print('Must wait until 2:30pm for tomorrows fuel prices')
+# if time <= '2 29':
+    # print('Must wait until 2:30pm for tomorrows fuel prices')
 # if selected_data_set == 2 ==
 
 # print(both_data_sets)
-
-
-
-#
-# for title in fuel_e.find_all('title'):
-#     values = []
-#     data.append(values)
-# #
-# def fuel_Brands(website):
-#     brandselect = input('Please select a brand: ',)
-#     brands = requests.get(website + '?Brand=' + str(brandselect))
-#     return feedparser.parse(brands.content)
-#
-# def fuel_Type(website):
-#     fueltype = input('Please select a type of fuel: ',)
-#     brands = requests.get(website + '?Product=' + str(fueltype))
-#     return feedparser.parse(brands.content)
-#
-# def fuel_Address(website):
-#     brands = requests.get(website + '?Brand=')
-#     return feedparser.parse(brands.content)
-#
-# def get_fuel_price(website):
-#     brandselect = input('Please select a brand')
-#     brands = requests.get(website + '?Brand=' + brandselect)
-#     return feedparser.parse(brands.content)
-# ##
