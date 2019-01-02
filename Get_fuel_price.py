a25e029365ead37250adb4c35e5eca8dc2ba2b1b
leviix <david27watters@gmail.com>
2019-01-02 19:31:18 +0800

    currently working version, still no control over what fuel product is being viewed, but both today and tomorrow work.....its been a long time since the last commit, now the code creates its own list of dictionaries and also sorts by the fuel price, the date has been added to be able to distinquish between them

--- Get_fuel_price.py
+++ Get_fuel_price.py
@@ -6,8 +6,10 @@ from bs4 import BeautifulSoup
 from pprint import pprint
 from time import localtime
 
-Fuelinfo_today = 'https://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=2'
-Fuelinfo_tomorrow = 'https://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=2&Day=tomorrow'
+
+
+Fuelinfo_today = 'https://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=6'
+Fuelinfo_tomorrow = 'https://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product=6&Day=tomorrow'
 FUELTEST = 'file:///Users/leviix/Desktop/Scripts/Python_scripts/Cheap_fuel/Fuelinformation.html'
 
 def menu():
@@ -30,7 +32,7 @@ def menu():
 
             elif selector == 3:
                 print('Getting both fuel prices...')
-                selected_data_set = [Fuelinfo_today, Fuelinfo_tomorrow]
+                selected_data_set = 'both'
                 return selected_data_set
 
             elif selector == 4:
@@ -45,44 +47,63 @@ def menu():
         except ValueError:
             print('Value Error: Please select 1 - 5')
             continue
-        # except ConnectionError:
-            # print('Not connected to the internet, check connection and try again')
-# menu()
-###
 
-combined_sets = []
+
+data_to_show = []
+
 def get_data():
-    selected_data_set = menu()
-    if selected_data_set == FUELTEST:
-        print(selected_data_set)
-        parsed_info = feedparser.parse(FUELTEST)
-
-    elif selected_data_set == []:
-        pass
-
-
+    """The main function being called"""
+    called_data = menu()
+    if called_data == 'both':
+        today_data = requests.get(Fuelinfo_today)
+        today_parsed = feedparser.parse(today_data.text)
+        tomorrow_data = requests.get(Fuelinfo_tomorrow)
+        tomorrow_parsed = feedparser.parse(tomorrow_data.text)
+        all_parsed = today_parsed.entries + tomorrow_parsed.entries
+        return showing(all_parsed)
+    elif called_data == FUELTEST:
+        print(called_data)
+        all_parsed = feedparser.parse(FUELTEST)
+        return showing(all_parsed.entries)
     else:
-        grabbed_data = requests.get(selected_data_set)
-        parsed_info = feedparser.parse(grabbed_data.text)
+        parse_this = requests.get(called_data)
+        all_parsed = feedparser.parse(parse_this.text)
+        return showing(all_parsed.entries)
 
-    for entries in reversed(parsed_info.entries):
-        print(entries.title, entries.address)
-        combined_sets.append(entries.title)
+
+def showing(show_this):
+    """Function to append to the list 'data_to_show' given an input of desired fuel day(s)"""
+    for entry in show_this:
+        data_to_show.append({
+            'Store_Name':entry['trading-name'],
+            'Price_of_Fuel':entry.price,
+            'Location':entry.location,
+            'Lat':entry.latitude,
+            'Long':entry.longitude,
+            'Address':entry.address,
+            'Date': entry.date,
+            })
+
 
 get_data()
 
+def sort_Price(data_to_show):
+    """sorts any list by the index 'price_of_fuel'"""
+    return data_to_show['Price_of_Fuel']
 
-# for entries in fuel_Formatted(grabbed_data).entries:
+
+Show_sorted = sorted(data_to_show, key=sort_Price, reverse=True)
+pprint(Show_sorted)
+print(len(data_to_show))
+
+
 
 print('--------------------     ENDED    --------------------')
-# print(localtime().tm_hour)
-# print(localtime().tm_min)
 hour = localtime().tm_hour
 minute = localtime().tm_min
-print('H',hour,': M', minute)
+second = localtime().tm_sec
+print('H:', hour, 'M:', minute, 'S:', second )
 
 # if time <= '2 29':
     # print('Must wait until 2:30pm for tomorrows fuel prices')
 # if selected_data_set == 2 ==
-
-# print(both_data_sets)
