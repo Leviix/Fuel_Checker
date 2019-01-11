f92fa764650a890e65fd97d29f17619d50e45840
leviix <david27watters@gmail.com>
2019-01-11 17:08:06 +0800

    commiting both the main application 'Get_fuel_price' and 'Product_menu' still working on everything product menu now has a completed menu, must select an item even to exit and no more random odd numbers currently working on rearranging the Main funciton in Fuel price

--- Get_fuel_price.py
+++ Get_fuel_price.py
@@ -16,55 +16,48 @@ FUELTEST = 'file:///Users/leviix/Desktop/Scripts/Python_scripts/Cheap_fuel/Fueli
 def moded_Url(prod, day):
     """meant to create and the required urls for both today and tomororw and return it"""
 
-    url = 'https://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product={prod}&Day={day}'
-
-
-
-    modded_url = url.format(prod= , day)
+    url = 'https://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?Product={product}&Day={days}'
+    modded_url = url.format(product=prod , days=day)
 
     return modded_url
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 data_to_show = []
 
 
 def get_data():
     """The main function being called.......aka Main"""
+    result_pM = prod_Men()
+    day_Z = ['Today', 'Tomorrow']
+    today_url = moded_Url(result_pM, day_Z[0])
+    tomorrow_url = moded_Url(result_pM, day_Z[1])
 
-    moded_Url(prod=prod_Men(), )
+    today_data = requests.get(today_url)
+    today_parsed = feedparser.parse(today_data.text)
+    tomorrow_data = requests.get(tomorrow_url)
+    tomorrow_parsed = feedparser.parse(tomorrow_data.text)
 
-    if  x == 'both':
-        today_data = requests.get(URl)
-        today_parsed = feedparser.parse(today_data.text)
-        tomorrow_data = requests.get(Fuelinfo_tomorrow)
-        tomorrow_parsed = feedparser.parse(tomorrow_data.text)
-        all_parsed = today_parsed.entries + tomorrow_parsed.entries
-        return showing(all_parsed)
-    elif called_data == FUELTEST:
-        print(called_data)
-        all_parsed = feedparser.parse(FUELTEST)
-        return showing(all_parsed.entries)
-    else:
-        parse_this = requests.get(called_data)
-        all_parsed = feedparser.parse(parse_this.text)
-        return showing(all_parsed.entries)
+    all_parsed = today_parsed.entries + tomorrow_parsed.entries
+
+    pprint(all_parsed)
+    print(len(all_parsed))
+    # if  x == 'both':
+    #     today_data = requests.get(URl)
+    #     today_parsed = feedparser.parse(today_data.text)
+    #     tomorrow_data = requests.get(Fuelinfo_tomorrow)
+    #     tomorrow_parsed = feedparser.parse(tomorrow_data.text)
+    #     all_parsed = today_parsed.entries + tomorrow_parsed.entries
+    #     return showing(all_parsed)
+    # elif called_data == FUELTEST:
+    #     print(called_data)
+    #     all_parsed = feedparser.parse(FUELTEST)
+    #     return showing(all_parsed.entries)
+    # else:
+    #     parse_this = requests.get(called_data)
+    #     all_parsed = feedparser.parse(parse_this.text)
+    #     return showing(all_parsed.entries)
+
+
+get_data()
 
 
 def showing(show_this):
@@ -90,8 +83,6 @@ Show_sorted = sorted(data_to_show, key=sort_Price, reverse=True)
 pprint(Show_sorted)
 print(len(data_to_show))
 
-
-get_data()
 
 
 print('--------------------     ENDED    --------------------')
--- Product_Menu.py
+++ Product_Menu.py
@@ -1,7 +1,8 @@
-import sys
+import os
 
 def product_Menu():
     """Menu for slecting Fuel product type, returns a value from dictionary 'options'."""
+    os.system('clear')
     while True:
         Base_url = 'https://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?'
         print('FUEL TYPES AVAILABLE')
@@ -11,12 +12,15 @@ def product_Menu():
                 4:4, 5:5,
                 6:10, 7:7}
         try:
-            print('FUELbalh something something yehb')
-            piked = int(input('no idea rn just press 1. 7 to exit: '))
+            print('')
+            piked = int(input('\nPlease select which fuel type you desire: '))
         except ValueError:
             print('Value Error: Please select 1 - 7')
             continue
         chosen = options.get(piked)
         if chosen == 7:
             sys.exit(0)
+        elif chosen == None:
+            continue
+
         return chosen
