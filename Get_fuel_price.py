f49424cae24268acee9cc6ceebf105e17f843243
leviix <david27watters@gmail.com>
2019-01-19 17:57:48 +0800

    Currently working on table creation, mucking around with for loops within the table creation function, not entirely sure if its the best way but oh well i guess im gonna try and make it work

--- Get_fuel_price.py
+++ Get_fuel_price.py
@@ -19,7 +19,7 @@ def moded_Url(prod, day):
 
 data_to_show = []
 
-def showing(show_this, colour):
+def showing(show_this, colour = 0):
     """Function to append to the list 'data_to_show' given an input of desired fuel day(s)"""
 
     if colour == 'Tomorrow':
@@ -46,6 +46,9 @@ def get_data():
     """The main function being called.......aka Main."""
 
     result_pM = prod_Men()
+    if result_pM == 8:
+        test_parsed = feedparser.parse(FUELTEST)
+        return showing(test_parsed)
     day_Z = ['Today', 'Tomorrow']
     today_url = moded_Url(result_pM, day_Z[0])
     tomorrow_url = moded_Url(result_pM, day_Z[1])
@@ -63,13 +66,67 @@ def get_data():
     colour_this = tomorrow_url[-8:]
     all_parsed = today_parsed.entries + tomorrow_parsed.entries
     showing(all_parsed, colour_this)
+    # table_Creation(all_parsed)
+    return all_parsed
 
-
-get_data()
+if __name__ == __name__:
+    get_data()
 
 Show_sorted = sorted(data_to_show, key=sort_Price, reverse=True)
-pprint(Show_sorted)
+# pprint(Show_sorted)
+
 print(len(data_to_show))
+print(Show_sorted)
+
+def table_Creation(data):
+
+    f = open('table.html', 'w')
+    table_stuff = '''
+    <table>
+        <thead>
+            <tr>
+                <th colspan = '5'> Fuel Scrapped</th>
+            </tr>
+            <tr>
+                <td> Store Name </td>
+                <td> Price </td>
+                <td> Address </td>
+                <td> Location </td>
+                <td> Date </td>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+                <td>{SN}</td>
+                <td>{P}</td>
+                <td>{A}</td>
+                <td>{L}</td>
+                <td>{D}</td>
+            </tr>
+        </tbody>
+    </table>
+    '''.format(SN='0', P='0', A='0', L='0', D='0' )
+    for entry in data:
+        print(entry)
+    title = '''
+        <head>
+            <title>
+                Fuel Scrapper
+            </title>
+        </head>
+        '''
+
+    body = '''
+        <h1>Fuel Scrapepd.</h1>
+        <p>Paragraph {}.</p>
+    '''.format(data)
+
+    f.write(table_stuff + title + body)
+    f.close()
+
+
+table_Creation(Show_sorted)
 
 
 
