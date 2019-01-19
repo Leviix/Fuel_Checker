f076a262b40d571c634c00ab14f6aa9175359279
leviix <david27watters@gmail.com>
2019-01-19 17:57:00 +0800

    added an 8th option witch is for the test file locally, only works if you know about if or accidently push it good enough for now

--- Product_Menu.py
+++ Product_Menu.py
@@ -7,10 +7,10 @@ def product_Menu():
     print('1:91 Petrol, 2:95 Unleaded, 3:98 unleaded, 4:Diesel, 5:LPG, 6:E85, 7:Quit')
     while True:
         Base_url = 'https://www.fuelwatch.wa.gov.au/fuelwatch/fuelWatchRSS?'
-        options = {1:1,
-                2:2, 3:6,
-                4:4, 5:5,
-                6:10, 7:7}
+        options = {1:1, 2:2,
+                3:6, 4:4,
+                5:5, 6:10,
+                7:7, 8:8}
         try:
             piked = int(input('\n\nPlease select which fuel type you desire: '))
         except ValueError:
